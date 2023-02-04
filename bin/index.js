720d630d3972ed9ee9829bf0de54dabdee69516f
Shinichi Okada <okada.shin@gmail.com>
2023-02-04 19:38:46 +0900

    feat: add -o --outputdir option

--- bin/index.js
+++ bin/index.js
@@ -1,17 +1,42 @@
 #!/usr/bin/env node
 
+const yargs = require('yargs');
 const sharp = require('sharp');
+const fs = require('fs');
+const path = require('path');
+const { version } = require('../package.json');
 
-const fileName = process.argv[2];
-const sizes = process.argv.slice(3).map(Number);
+const argv = yargs
+  .version(version)
+  .usage('Usage: resize-image <file> <size1> [<size2> [<size3> ...]]')
+  .options({
+    outputdir: {
+      alias: 'o',
+      description: 'The directory to save the resized images',
+      type: 'string'
+    }
+  })
+  .help()
+  .argv;
 
-if (!fileName || !sizes.length) {
+const fileName = argv._[0];
+const sizes = argv._.slice(1).map(size => Number(size));
+
+const outputdir = argv.outputdir || '.';
+
+const validSizes = sizes.filter(size => !isNaN(size) && size > 0);
+
+if (!fileName || !validSizes.length) {
   console.error('Usage: resize-image <file> <size1> [<size2> [<size3> ...]]');
   process.exit(1);
 }
 
+if (!fs.existsSync(outputdir)) {
+  fs.mkdirSync(outputdir);
+}
+
 Promise.all(sizes.map(async size => {
-  const resizedFileName = `${size}_${fileName}`;
+  const resizedFileName = path.join(outputdir, `${size}_${fileName}`);
   await sharp(fileName)
     .resize({ width: size })
     .toFile(resizedFileName);
